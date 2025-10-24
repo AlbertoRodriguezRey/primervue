d7e4027af428e336c934328b599fb4b9a4b3fd15
AlbertoRodriguezRey <alberto.rodriguez-rey@tajamar365.com>
2025-10-24 09:54:08 +0200

    Mi primer VUE

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Mi primer vue de viernes, que ilusion!!!</h1>
+  </div>
+</template>
+
+<script>
+//VUE UTILIZA CODIGO JS (ESPECIAL)
+//CADA COMPONENT QUE GENEREMOS DEBE TENER UN NOMBRE OBLIGATORIO
+//Y DEBEMOS EXPORTARLO CON DICHO NOMBRE. EL name ES LA ETIQUETA QUE
+//UTILIZARAN OTROS COMPONENTES PARA DIBUJARLO
+export default {
+  name: "HolaMundo"
+};
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
